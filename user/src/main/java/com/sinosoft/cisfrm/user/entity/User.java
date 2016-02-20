ed677492bd81f76e09553e420a0659e05e8ae83d
wangchg <wangchg@hotmail.com>
2016-02-20 14:43:40 +0800

    jpa

--- user/src/main/java/com/sinosoft/cisfrm/user/entity/User.java
+++ user/src/main/java/com/sinosoft/cisfrm/user/entity/User.java
@@ -1,23 +1,25 @@
 package com.sinosoft.cisfrm.user.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.Table;
 import java.io.Serializable;
 
 /**
  * Created by Dawn on 16/2/19.
  */
-
+@Entity
+@Table()
 public class User implements Serializable{
-    private int id;
+    private int moduleId;
     private String username;
     private String password;
 
-    public int getId() {
-        return id;
+    public int getModuleId() {
+        return moduleId;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void setModuleId(int moduleId) {
+        this.moduleId = moduleId;
     }
 
     public String getUsername() {
